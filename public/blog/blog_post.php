96aaed16e4fdf47b8301d473f69e76e12573f33d
Mayumi G <mayumi18mg@gmail.com>
2020-12-25 22:02:59 -0500

    バリデーションで使ったヘッダーロケーションに記事のidを足しました。

--- public/blog/blog_post.php
+++ public/blog/blog_post.php
@@ -14,6 +14,7 @@ if (!$_SESSION['login']) {
 //--------------------------------
 require_once './../../private/database.php';
 require_once './../../private/functions.php';
+
 //お知らせの隣に表示させる未読のコメント数
 $UnreadCommentCount = getCommentCount($users_id, 0);
 
@@ -35,103 +36,104 @@ $UnreadCommentCount = getCommentCount($users_id, 0);
        <?php include './../../header.php';?>
 
        <label for="check">
-        <div class="wrapper">
-            <div class="container">
-                 <div class="typein">
+            <div class="wrapper">
+                <div class="container">
+                    <div class="typein">
 
-                      <h2 class="form_title">記事投稿フォーム</h2>
-                         <div class="error_msg">
-                            <?php if (isset($_GET["error"])):?>
+                        <h2 class="form_title">記事投稿フォーム</h2>
+                            <div class="error_msg">
+                                <?php if (isset($_GET["error"])):?>
 
-                                <?php if ($_GET["error"]=="invalid_title"):?>
-                                <p><?php echo "タイトルを入力してください。";?></p>
+                                    <?php if ($_GET["error"]=="invalid_title"):?>
+                                    　　<p><?php echo "タイトルを入力してください。";?></p>
+                                    <?php endif;?>
+
+                                    <?php if ($_GET["error"]=="invalid_title_lengh"):?>
+                                    　　<p><?php echo "タイトルは25字以下にして下さい。";?></p>
+                                    <?php endif;?>
+
+                                    <?php if ($_GET["error"]=="invalid_content"):?>
+                                    　　<p><?php echo "本文を入力して下さい。";?></p>
+                                    <?php endif;?>
+
+                                    <?php if ($_GET["error"]=="invalid_category"):?>
+                                        <p><?php echo "カテゴリーは必須です。";?></p>
+                                    <?php endif;?>
+
+                                    <?php if ($_GET["error"]=="invalid_status"):?>
+                                        <p><?php echo "公開ステータスは必須です。";?></p>
+                                    <?php endif;?>
+
+                                    <?php if ($_GET["error"]=="invalid_caption"):?>
+                                        <p><?php echo "キャプションを入力して下さい";?></p>
+                                    <?php endif;?>
+
+                                    <?php if ($_GET["error"]=="invalid_file"):?>
+                                        <p><?php echo "キャプションをつける画像がありません。";?></p>
+                                    <?php endif;?>
+
+                                    <?php if ($_GET["error"]=="invalid_caption_length"):?>
+                                        <p><?php echo "キャプションは140文字以内で入力して下さい。";?></p>
+                                    <?php endif;?>
+
+                                    <?php if ($_GET["error"]=="invalid_filesize"):?>
+                                        <p><?php echo "ファイルサイズは１MB未満にして下さい。";?></p>
+                                    <?php endif;?>
+
+                                    <?php if ($_GET["error"]=="invalid_file"):?>
+                                        <p><?php echo "画像ファイルを添付して下さい。";?></p>
+                                    <?php endif;?>
+
+
                                 <?php endif;?>
+                            </div><!--error_msg-->
 
-                                <?php if ($_GET["error"]=="invalid_title_lengh"):?>
-                                <p><?php echo "タイトルは25字以下にして下さい。";?></p>
-                                <?php endif;?>
+            　　　　　　　　　　　<!--enctype="multipart/form-data" は画像ファイルのためにformタグに付ける決まり文句-->
+                            <form action="./blog_posted.php" method="POST" enctype="multipart/form-data" >
 
-                                <?php if ($_GET["error"]=="invalid_content"):?>
-                                <p><?php echo "本文を入力して下さい。";?></p>
-                                <?php endif;?>
+                                    <div class="form_item"><p>タイトル</p></div>
+                                        <input type="text" class="form_text" name="title">
 
-                                <?php if ($_GET["error"]=="invalid_category"):?>
-                                    <p><?php echo "カテゴリーは必須です。";?></p>
-                                <?php endif;?>
+                                    <div class="form_item"><p>ブログ本文</p></div>
+                                        <textarea name="content" id="content" cols="100" rows="20"></textarea>
+                                    <br>
 
-                                <?php if ($_GET["error"]=="invalid_status"):?>
-                                    <p><?php echo "公開ステータスは必須です。";?></p>
-                                <?php endif;?>
+                                    <div class="form_item"><p>カテゴリ</p></div>
+                                        <select name="category">
+                                            <option value=1>テーマ１</option>
+                                            <option value=2>テーマ２</option>
+                                            <option value=3>その他</option>
+                                        </select>
+                                    <br>
+                                    <br>
 
-                                <?php if ($_GET["error"]=="invalid_caption"):?>
-                                    <p><?php echo "キャプションを入力して下さい";?></p>
-                                <?php endif;?>
+                                    <div class="form_item"></div>
+                                        <input class="radio" type="radio" name="publish_status" value="1" checked>公開
+                                        <input class="radio" type="radio" name="publish_status" value="2">非公開
+                                    <br>
+                                    <br>
 
-                                <?php if ($_GET["error"]=="invalid_file"):?>
-                                    <p><?php echo "キャプションをつける画像がありません。";?></p>
-                                <?php endif;?>
+                                    <div class="form_item"><p>画像</p></div>
+                                    　<input name="img" type="file" accept="image/*"/><br>
+                                    　<input type="hidden" name="MAX_FILE_SIZE" value="1048576" />
+                                    <br>
+                                    
+                                    <textarea
+                                        class="caption"
+                                        name="caption"
+                                        placeholder="キャプション（140文字以下）"
+                                        id="caption" cols="50" rows="10"
+                                    ></textarea>
+                                    <br>
+                                    <br>
 
-                                <?php if ($_GET["error"]=="invalid_caption_length"):?>
-                                    <p><?php echo "キャプションは140文字以内で入力して下さい。";?></p>
-                                <?php endif;?>
-
-                                <?php if ($_GET["error"]=="invalid_filesize"):?>
-                                    <p><?php echo "ファイルサイズは１MB未満にして下さい。";?></p>
-                                <?php endif;?>
-
-                                <?php if ($_GET["error"]=="invalid_file"):?>
-                                    <p><?php echo "画像ファイルを添付して下さい。";?></p>
-                                <?php endif;?>
-
-
-                            <?php endif;?>
-                        </div><!--error_msg-->
-
-        　　　　　　　　　　　<!--enctype="multipart/form-data" は画像ファイルのためにformタグに付ける決まり文句-->
-                        <form action="./blog_posted.php" method="POST" enctype="multipart/form-data" >
-
-                                <div class="form_item"><p>タイトル</p></div>
-                                    <input type="text" class="form_text" name="title">
-
-                                <div class="form_item"><p>ブログ本文</p></div>
-                                    <textarea name="content" id="content" cols="100" rows="20"></textarea>
-                                <br>
-
-                                <div class="form_item"><p>カテゴリ</p></div>
-                                    <select name="category">
-                                        <option value=1>テーマ１</option>
-                                        <option value=2>テーマ２</option>
-                                        <option value=3>その他</option>
-                                    </select>
-                                <br>
-                                <br>
-
-                                <div class="form_item"></div>
-                                    <input class="radio" type="radio" name="publish_status" value="1" checked>公開
-                                    <input class="radio" type="radio" name="publish_status" value="2">非公開
-                                <br>
-                                <br>
-
-                                <div class="form_item"><p>画像</p></div>
-                                <input name="img" type="file" accept="image/*"/><br>
-                                <input type="hidden" name="MAX_FILE_SIZE" value="1048576" />
-                                <br>
-                                <textarea
-                                    class="caption"
-                                    name="caption"
-                                    placeholder="キャプション（140文字以下）"
-                                    id="caption" cols="50" rows="10"
-                                ></textarea>
-                                <br>
-                                <br>
-
-                                <input type="submit" value="投稿" class="btn">
-                        </form>
+                                    <input type="submit" value="投稿" class="btn">
+                            </form>
 
 
-                </div><!--typein-->
-            </div><!--container-->
-        </div> <!--wrapper-->
+                    </div><!--typein-->
+                </div><!--container-->
+            </div> <!--wrapper-->
       </label>
     </body>
 </html>
